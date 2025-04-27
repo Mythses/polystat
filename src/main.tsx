0aee98b948a961a42411063d54d5311d41d106b2
lewis <lewishelh@gmail.com>
2025-04-26 19:16:06 -0700

    wtf pls work omfg

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import StatsViewer from "./StatsViewer.tsx";
+import StatsViewer from "./StatsViewer";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
